28ffa6015beb4213f6e6f5ed02e06827413ca401
SergioMorenoAntequera <seranmoreno500@gmail.com>
2020-08-28 12:21:51 +0200

    Cleaning code and moving floating button to total

--- lib/screens/mainPage.dart
+++ lib/screens/mainPage.dart
@@ -12,13 +12,6 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int amount025 = 0;
-  int amount033 = 0;
-  int amount050 = 0;
-  int amount100 = 0;
-  double _totalDrinks = 0;
-
-  // static double totalAmount = 0;
   // We need to pass a map to use it as a
   static Map<String, double> totalCuantity = {'value': 0};
 
@@ -33,11 +26,12 @@ class _MainPageState extends State<MainPage> {
       title: Text("Hangover Calculator"),
     );
 
+    //Button to Reset
     Container fab = new Container(
-      height: 85,
-      width: 85,
+      height: 70,
+      width: 70,
       child: Padding(
-        padding: EdgeInsets.all(8),
+        padding: EdgeInsets.all(0),
         child: FloatingActionButton(
           onPressed: resetDrinks,
           backgroundColor: Theme.of(context).backgroundColor,
@@ -52,18 +46,18 @@ class _MainPageState extends State<MainPage> {
 
     return Scaffold(
       appBar: appbar,
-      floatingActionButton: fab,
+      // floatingActionButton: fab,
       backgroundColor: Theme.of(context).backgroundColor,
 
       // body:
       body: Stack(
         children: <Widget>[
-          // App background I guess
           Container(
             child: ListView(
               children: <Widget>[
                 Stack(
                   children: [
+                    // App background at the top
                     Container(
                       color: Theme.of(context).primaryColor,
                       height: 160,
@@ -99,27 +93,6 @@ class _MainPageState extends State<MainPage> {
                           key: UniqueKey(),
                           beer: beer100,
                         ),
-                        // BeerTile(
-                        //   key: UniqueKey(),
-                        //   capacity: 0.33,
-                        //   addAction: addDrinks,
-                        //   removeAction: removeDrinks,
-                        //   cuantity: amount033,
-                        // ),
-                        // BeerTile(
-                        //   key: UniqueKey(),
-                        //   capacity: 0.50,
-                        //   addAction: addDrinks,
-                        //   removeAction: removeDrinks,
-                        //   cuantity: amount050,
-                        // ),
-                        // BeerTile(
-                        //   key: UniqueKey(),
-                        //   capacity: 1.00,
-                        //   addAction: addDrinks,
-                        //   removeAction: removeDrinks,
-                        //   cuantity: amount100,
-                        // ),
                         SizedBox(
                           height: 115,
                         ),
@@ -131,7 +104,7 @@ class _MainPageState extends State<MainPage> {
             ),
           ),
 
-          // Position absolute Thingy
+          // Total bar
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
@@ -139,6 +112,8 @@ class _MainPageState extends State<MainPage> {
               padding:
                   EdgeInsets.only(top: 20, bottom: 20, left: 13, right: 13),
               child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 // Total y total de litros a la izquierda
                 children: <Widget>[
                   Container(
@@ -171,6 +146,10 @@ class _MainPageState extends State<MainPage> {
                       ],
                     ),
                   ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: fab,
+                  ),
                 ],
               ),
             ),
@@ -180,86 +159,13 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  void addDrinks(double amount, int cuantity) {
-    // setState(() {
-    //   _totalDrinks += amount;
-    //   switch (amount.toString()) {
-    //     case "0.25":
-    //       amount025 += cuantity;
-    //       break;
-    //     case "0.33":
-    //       amount033 += cuantity;
-    //       break;
-    //     case "0.5":
-    //       amount050 += cuantity;
-    //       break;
-    //     case "1.0":
-    //       amount100 += cuantity;
-    //       break;
-    //     default:
-    //   }
-    // });
-  }
-
-  void removeDrinks(double amount, int cuantity) {
-    setState(() {
-      if (_totalDrinks >= amount) {
-        switch (amount.toString()) {
-          case "0.25":
-            if (amount025 - cuantity >= 0) {
-              amount025 -= cuantity;
-              _totalDrinks -= amount;
-            }
-            break;
-          case "0.33":
-            if (amount033 - cuantity >= 0) {
-              amount033 -= cuantity;
-              _totalDrinks -= amount;
-            }
-            break;
-          case "0.5":
-            if (amount050 - cuantity >= 0) {
-              amount050 -= cuantity;
-              _totalDrinks -= amount;
-            }
-            break;
-          case "1.0":
-            if (amount100 - cuantity >= 0) {
-              amount100 -= cuantity;
-              _totalDrinks -= amount;
-            }
-            break;
-          default:
-        }
-      }
-    });
-    if (amount025 == 0 && amount033 == 0 && amount050 == 0 && amount100 == 0) {
-      _totalDrinks = 0;
-    }
-  }
-
   resetDrinks() {
     setState(() {
-      _totalDrinks = 0;
-      // totalAmount['value'] = 0;
+      totalCuantity['value'] = 0;
       beer025.amount = 0;
       beer033.amount = 0;
       beer050.amount = 0;
       beer100.amount = 0;
-      // beer025.totalAmount = 0;
-      amount025 = 0;
-      amount033 = 0;
-      amount050 = 0;
-      amount100 = 0;
     });
-  }
-
-  // To avoid problems with periodic numbers
-  String formatTotal() {
-    if (this._totalDrinks.toString().length < 12)
-      return this._totalDrinks.toString();
-
-    int indexAux = this._totalDrinks.toString().indexOf(".");
-    return this._totalDrinks.toString().substring(0, indexAux + 3);
   }
 }
